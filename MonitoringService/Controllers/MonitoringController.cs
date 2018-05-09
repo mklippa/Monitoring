e43e1fac397b5093a11cb338e25bcb2c4332229c
Maxim Klippa <mklippa@oneincsystems.com>
2018-05-09 12:31:19 +0700

    init monitoring service

---
+++ MonitoringService/Controllers/MonitoringController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MonitoringService.Controllers
+{
+    [Route("api/[controller]")]
+    public class MonitoringController : Controller
+    {
+        [HttpPost]
+        [Route("agents/{id}/errors")]
+        public IActionResult SaveErrors(string id, [FromBody] string[] errors)
+        {
+            try
+            {
+                return StatusCode((int) HttpStatusCode.OK);
+            }
+            catch
+            {
+                return StatusCode((int) HttpStatusCode.InternalServerError);
+            };
+        }
+    }
+}
