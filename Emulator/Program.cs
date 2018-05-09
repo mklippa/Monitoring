fc9e3f026c2b89466ee6734f7551323b39f70de6
Maxim Klippa <mklippa@oneincsystems.com>
2018-05-09 09:31:40 +0700

    init emulator, add git ignore

---
+++ Emulator/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+
+namespace Emulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Enter emulator number
+            var count = 0;
+            while (true)
+            {
+                Console.WriteLine("Please, enter the correct emulator number:");
+                if (int.TryParse(Console.ReadLine(), out count))
+                {
+                    break;
+                }
+            }
+
+            // Prepare agents
+            var agents = new Agent[count];
+
+            for (var i = 0; i < agents.Length; i++)
+            {
+                agents[i] = new Agent(i + 1);
+            }
+
+            // Run emulation
+            while (true)
+            {
+                for (var i = 0; i < agents.Length; i++)
+                {
+                    agents[i].SentReport();
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+
+    public class Agent
+    {
+        public Agent(int id)
+        {
+            Id = id;
+        }
+
+        public int Id { get; }
+
+        public void SentReport()
+        {
+            Console.WriteLine($"Agent {Id} send report.");
+        }
+    }
+}
